044140f044e6c98d7979bd2f2fcfcc8ff89474d4
VedikaSingh1 <vedikasingh3153@gmail.com>
2024-02-18 11:33:09 +0530

    second commit api key added

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '46146d15-9dda-426f-b558-a72451a104d7';
 export const MODAL_CLOSE_SEC = 2.5;
+//https://forkify-api.herokuapp.com/v2
